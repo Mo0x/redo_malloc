e979f7455636df2630ecd01e61ff5ebc23758909
xam <max.govindaraju@gmail.com>
2026-02-17 16:42:01 +0100

    add: is_power_of_two in malloc.c

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,9 +1,17 @@
 #include "libft_malloc.h"
+#include <stdint.h>
 
 /*
+  doc: https://www.gingerbill.org/series/memory-allocation-strategies/
   TODO find ideal size for TINY, SMALL and LARGE, should be a multiple of 4096
   EACH zones must contain at least 100 allocations
 */
+
+// idk if it will be usefull
+int  is_power_of_2(uintptr_t x)
+{
+  return (x & (x - 1)) == 0;
+}
 
 void  *malloc(size_t size)
 {
@@ -20,4 +28,3 @@ void  *malloc(size_t size)
     //LARGE
   }
 }
-
