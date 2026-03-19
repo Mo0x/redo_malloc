291089855a773d70cb5395cdb1a6625fd3c15be5
xam <max.govindaraju@gmail.com>
2026-03-20 00:54:03 +0100

    starting on large alloc inside malloc

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -20,27 +20,44 @@ void  *malloc(size_t size)
 {
 	t_global_allocator *alloc = get_alloc();
 	int flag_fail = 0; 
+	void *ret_ptr = NULL;
+	void *header_ptr = NULL;
+
 	if (!alloc->is_init)
 	{
 		flag_fail = init_global_allocator(alloc);
 		if (flag_fail == -1)
 		{
 			//TODO set errno here, will have to check which one it is
-			return NULL;
+			return ret_ptr;
 		}
 	}
 	if (size < alloc->n)
 	{
 		//tiny zone
 		write(2, "Not operational yet\n", ft_strlen("Not operational yet\n"));
+		return ret_ptr;
 	}
 	else if (size < alloc->m)
 	{
 		//small zone
 		write(2, "Not operational yet\n", ft_strlen("Not operational yet\n"));
+		return ret_ptr;
 	}
 	else
-	{
+	{		
 		//large zone
+		header_ptr = mmap(header+ptr, size, ?????);
+		if (!alloc->large_head)
+		{
+			header_ptr = init_zone(alloc->large_head); //future function that will initiate the head of a zone
+			
+		}
+		else
+		{
+			//some list add back of some kind 
+
+		}		
+		
 	}
 }
